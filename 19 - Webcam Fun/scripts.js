b9aba720edfe556754af5c2f68c56673e3b23e2a
RazaqHimawan <112485259+RazaqHimawan@users.noreply.github.com>
2023-01-19 18:22:46 +0700

    add file to chapter 19

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,30 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+const getVideo = ()=>{
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream =>{
+        console.log(localMediaStream)
+        video.src = window.URL.createObjectURL(localMediaStream)
+        video.play()
+       })
+       .catch(err=>{
+        console.error(`Oh NO`,err)
+       })
+}
+
+const paintToCanvas = () =>{
+    const width = video.videoWidth
+    const height = video.videoHeight
+    canvas.width = width
+    canvas.height = height
+
+    setInterval(()=>{
+        ctx.drawImage(video, 0, 0, width, height)
+    },16)
+}
+
+getVideo()
